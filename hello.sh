6c9e95337a01dd3804e04e9d95f7fb916fbf12c8
jibin jose <jibinjose884@gmail.com>
2026-03-01 23:01:52 +0530

    Added shell hello program

---
+++ hello.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+echo "Hello, World!"
+echo "Welcome to Shell Programming"
+
+# -------------------------------
+# Running Commands:
+# chmod +x hello.sh
+# ./hello.sh
+#
+# Output:
+# Hello, World!
+# Welcome to Shell Programming
+# -------------------------------
