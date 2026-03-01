b3133df84d5c81686668d12120fa6bdd91e9d78c
jibin jose <jibinjose884@gmail.com>
2026-03-01 23:07:18 +0530

    add user input program using read command

---
+++ user.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+echo "Enter Your name:"
+read name
+
+echo "Welcome $name"
+
+# -------------------------------
+# Running Commands:
+# chmod +x user.sh
+# ./user.sh
+#
+# Example Output:
+# Enter Your name:
+# Jibin
+# Welcome Jibin
+# -------------------------------
