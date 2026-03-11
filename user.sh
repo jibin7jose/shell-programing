086c8df7cddcd5b98737e83d25e03a1d57b997cb
jibin jose <jibinjose884@gmail.com>
2026-03-11 22:36:54 +0530

    Update user.sh

--- user.sh
+++ user.sh
@@ -1,17 +1,26 @@
 #!/bin/bash
 
+: '
+-----------------------------------------
+File Name: user.sh
+Description: Shell script to take user input and display a welcome message
+Author: Jibin Jose
+-----------------------------------------
+
+Running Commands:
+chmod +x user.sh
+./user.sh
+
+Example Output:
+Enter Your name:
+Jibin
+Welcome Jibin
+-----------------------------------------
+'
+
+# Ask user to enter name
 echo "Enter Your name:"
 read name
 
+# Print welcome message
 echo "Welcome $name"
-
-# -------------------------------
-# Running Commands:
-# chmod +x user.sh
-# ./user.sh
-#
-# Example Output:
-# Enter Your name:
-# Jibin
-# Welcome Jibin
-# -------------------------------
