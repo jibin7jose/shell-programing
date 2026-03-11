a530d050dd7a0ae1ac69473f04d4f8f46fcf789b
jibin jose <jibinjose884@gmail.com>
2026-03-11 22:12:49 +0530

    Update hello.sh

--- hello.sh
+++ hello.sh
@@ -1,14 +1,22 @@
 #!/bin/bash
 
+: '
+-----------------------------------------
+File Name: hello.sh
+Description: Shell script to print Hello World and a welcome message
+Author: Jibin Jose
+-----------------------------------------
+
+Running Commands:
+chmod +x hello.sh
+./hello.sh
+
+Example Output:
+Hello, World!
+Welcome to Shell Programming
+-----------------------------------------
+'
+
+# Print messages
 echo "Hello, World!"
 echo "Welcome to Shell Programming"
-
-# -------------------------------
-# Running Commands:
-# chmod +x hello.sh
-# ./hello.sh
-#
-# Output:
-# Hello, World!
-# Welcome to Shell Programming
-# -------------------------------
